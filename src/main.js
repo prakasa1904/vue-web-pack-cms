438744259a01d2ea45b583aa728af97edebb9bff
Nedya Amrih Praksa <nedya@detik.com>
2017-01-23 16:46:41 +0700

    Disable Comment

--- src/main.js
+++ src/main.js
@@ -1,5 +1,4 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+// Disable Comment Only
 import Vue from 'vue'
 import Vuex from 'vuex'
 import VueRouter from 'vue-router'
